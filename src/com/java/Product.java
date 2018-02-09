66556549e15bb6093f341b51c283527219e6d697
极客慧 <1003669794@qq.com>
2018-02-09 17:12:36 +0800

    修改商品信息

--- src/com/java/Product.java
+++ src/com/java/Product.java
@@ -1,4 +1,11 @@
 package com.java;
 
 public class Product {
+
+    private String color;
+    private String price;
+
+    public static void main(String[] args) {
+        System.out.println("商品信息");
+    }
 }
