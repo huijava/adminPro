b2b90bd85264f7269b35117afa5beb0bb18c5f3b
极客慧 <1003669794@qq.com>
2018-02-09 18:12:59 +0800

    获取用户信息

--- src/com/java/run.java
+++ src/com/java/run.java
@@ -3,8 +3,7 @@ package com.java;
 public class run {
 
     public static void main(String[] args) {
-        Product product = new Product("极客慧", 20L);
-        System.out.println(product.toString());
+        System.out.println("获取用户信息");
     }
 
 }
