180627bcde5de93871f02f1c6b211c739adb86fb
极客慧 <1003669794@qq.com>
2018-02-09 18:20:11 +0800

    计算用户订单价格

--- src/com/java/run.java
+++ src/com/java/run.java
@@ -5,6 +5,7 @@ public class run {
     public static void main(String[] args) {
         System.out.println("获取订单信息");
         System.out.println("获取用户信息");
+        System.out.println("计算用户订单价格");
     }
 
 }
